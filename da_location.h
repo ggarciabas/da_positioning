d0163ad0ef51b5201409d0b2464b95ff25e276e2
Giovanna Garcia <ggarciabas@gmail.com>
2019-02-20 10:13:55 -0300

    Testando da_location

---
+++ da_location.h
@@ -0,0 +1,325 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <istream>
+#include <time.h>
+
+void PrintMatrix (std::vector<std::vector<long double> > matrix, std::string name)
+{
+  std::ofstream file;
+  file.open(name, std::ofstream::out);
+  for (std::vector<std::vector<long double> >::iterator i = matrix.begin(); i != matrix.end(); ++i)
+  {
+    std::vector<long double>::iterator j = (*i).begin();
+    file << (*j);
+    j++;
+    for (; j != (*i).end(); ++j)
+    {
+      file << "," << (*j);
+    }
+    file << std::endl;
+  }
+  file.close();
+}
+
+std::vector<std::vector<long double> > positioning (std::vector<std::vector<long double> > b_ij, int N, std::string path) {
+  // normalize bij
+  double max = 0;
+  for (unsigned i = 0; i < N; ++i) // UAV
+  {
+    for (unsigned j = 0; j < N; ++j) // LOC
+    {
+      if (b_ij[i][j] > max) {
+        max = b_ij[i][j];
+      }
+    }
+  }
+  for (unsigned i = 0; i < N; ++i) // UAV
+  {
+    for (unsigned j = 0; j < N; ++j) // LOC
+    {
+      b_ij[i][j] /= max;
+    }
+  }
+
+  std::ostringstream os;
+  os << path << "/bij_norm.txt";
+  PrintMatrix (b_ij, os.str());
+
+  srand(time(NULL));
+  double temp = 0.9;
+  // Mai
+  std::vector<std::vector<long double> > m_ij;
+  // variavel da tranformacao algebrica (parte do self-amplification)
+  std::vector<std::vector<long double> > o_ij;
+  // variavel da transformacao algebrica (parte o Y_sch)
+  std::vector<std::vector<long double> > lamb_ij;
+  // q_ai
+  std::vector<std::vector<long double> > q_ij;
+  // Ptr<UniformRandomVariable> e_ai = CreateObject<UniformRandomVariable>(); // Padrão [0,1]
+  // e_ai->SetAttribute ("Min", DoubleValue (min));
+  double rdom;
+
+  for (unsigned i = 0; i < N; ++i) // UAV
+  {
+    m_ij.push_back(std::vector<long double>());
+    o_ij.push_back(std::vector<long double>());
+    lamb_ij.push_back(std::vector<long double>());
+    q_ij.push_back(std::vector<long double>());
+    for (unsigned j = 0; j < N; ++j) // LOC
+    {
+      // rdom = (double)rand()/RAND_MAX; // [0,1]
+      m_ij[i].push_back(1.0/(double)N); // + rdom);
+      o_ij[i].push_back(m_ij[i][j]);
+      lamb_ij[i].push_back(0.0);
+      q_ij[i].push_back(0.0);
+    }
+  }
+
+  int print = 0;
+  os.str("");
+  os << path << "/I_mij.txt";
+  PrintMatrix (m_ij, os.str());
+
+  unsigned itB_max = 1000;
+  unsigned itA_max = 1000;
+  unsigned itC_max = 500;
+  double gamma = 0.99;
+
+  // Part A
+  unsigned itA = 0;
+  long double new_mij;
+  bool converge_B = true, converge_C = true; // para saber se houve mudanca no valor
+  while (temp >= 0.005 && itA < itA_max)
+  {
+    // Part B
+    unsigned itB = 0;
+    // do
+    // {
+    //   converge_B = true;
+      
+      for (unsigned i = 0; i < N; ++i)
+      {
+        for (unsigned j = 0; j < N; ++j)
+        {
+          // calculate \lamb_{ij}
+          // NS_LOG_DEBUG ("MIJ [" << i << "," << j << "] : " << m_ij[i][j]);
+          lamb_ij[i][j] = m_ij[i][j] * b_ij[i][j];
+          if (isnan(lamb_ij[i][j])) {
+            os.str("");
+            os << path << "/B_q_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (q_ij, os.str());
+            os.str("");
+            os << path << "/B_o_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (o_ij, os.str());
+            os.str("");
+            os << path << "/B_lamb_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (lamb_ij, os.str());
+            os.str("");
+            os << path << "/B_m_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (m_ij, os.str());
+            std::cout << "Lab is nan: ["<<i<<","<<j<<"] " << m_ij[i][j] << " * " << b_ij[i][j] << " = " << lamb_ij[i][j] << "   " << itB;
+            exit(0);
+          }
+          // calculate Q_{ij}
+          q_ij[i][j] =  gamma * o_ij[i][j] - lamb_ij[i][j] * b_ij[i][j];
+          if (isnan(q_ij[i][j])) {
+            os.str("");
+            os << path << "/B_q_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (q_ij, os.str());
+            os.str("");
+            os << path << "/B_o_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (o_ij, os.str());
+            os.str("");
+            os << path << "/B_lamb_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (lamb_ij, os.str());
+            os.str("");
+            os << path << "/B_m_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (m_ij, os.str());
+            std::cout << "qij is nan:  " << itB;
+            exit(0);
+          }
+          // calculate m_{ij}
+          new_mij = expl(q_ij[i][j] / (long double) temp);
+          if (m_ij[i][j] != new_mij) { // mudou!
+            converge_B = false;
+            converge_C = false;
+          }
+          m_ij[i][j] = new_mij;
+          if (isnan(m_ij[i][j])) {
+            os.str("");
+            os << path << "/B_q_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (q_ij, os.str());
+            os.str("");
+            os << path << "/B_o_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (o_ij, os.str());
+            os.str("");
+            os << path << "/B_lamb_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (lamb_ij, os.str());
+            os.str("");
+            os << path << "/B_m_ij_" << std::setfill ('0') << std::setw (4) << itA << "_" << itB << ".txt";
+            PrintMatrix (m_ij, os.str());
+            std::cout << "mij is nan  " << itB;
+            exit(0);
+          }
+        }
+      }
+
+      // Part C
+      // unsigned itC = 0;
+      // os.str("");
+      // do
+      // {
+      //   converge_C = true;      
+      //   unsigned k = 0;
+      //   long double total;
+      //   for (unsigned i = 0; i < N; ++i)
+      //   {
+      //     total = 0.0;
+      //     for (k = 0; k < N; ++k)
+      //     {
+      //       total += m_ij[i][k];
+      //     }
+      //     for (k = 0; k < N; ++k)
+      //     {
+      //       new_mij = m_ij[i][k] / total;
+      //       if (m_ij[i][k] != new_mij) {
+      //         converge_B = converge_C = false;
+      //       }
+      //       m_ij[i][k] = new_mij;
+      //     }
+      //   }
+
+      //   for (unsigned i = 0; i < N; ++i)
+      //   {
+      //     total = 0.0;
+      //     for (k = 0; k < N; ++k)
+      //     {
+      //         total += m_ij[k][i];
+      //     }
+      //     for (k = 0; k < N; ++k)
+      //     {
+      //         new_mij = m_ij[k][i] / total;
+      //         if (m_ij[k][i] != new_mij) {
+      //             converge_B = converge_C = false;
+      //         }
+      //         m_ij[k][i] = new_mij;
+      //     }
+      //   }
+      //   itC++;
+      // } while (!converge_C && itC < itC_max);
+
+    //   itB++;
+    // } while (!converge_B && itB < itB_max);
+
+    // os.str("");
+    // os << path << "/A_mij_" << std::setfill ('0') << std::setw (7) << itA << ".txt";
+    // PrintMatrix (m_ij, os.str());
+    // os.str("");
+    // os << path << "/A_lamb_ij_" << std::setfill ('0') << std::setw (7) << itA << ".txt";
+    // PrintMatrix (lamb_ij, os.str());
+    // os.str("");
+    // os << path << "/A_q_ij_" << std::setfill ('0') << std::setw (7) << itA << ".txt";
+    // PrintMatrix (q_ij, os.str());
+
+    // long double sum = 0.0, lin=0, col=0;
+    // for (unsigned i = 0; i < N; ++i)
+    // {
+    //   lin=0;
+    //   for (unsigned j = 0; j < N; ++j)
+    //   {
+    //     sum += m_ij[i][j] * b_ij[i][j];
+    //     lin += m_ij[i][j] * b_ij[i][j];
+    //   }
+    //   if (lin > 1.0 || lin < 1.0) {
+    //     std::cout << "lin:" << lin << "\t";
+    //   }
+
+    //   col=0;
+    //   for (unsigned k = 0; k < N; ++k)
+    //   {
+    //     col += m_ij[k][i] * b_ij[k][i];
+    //   }
+    //   if (col > 1.0 || col < 1.0) {
+    //     std::cout << "col:" << col << "\t";
+    //   }
+    // }
+
+    // std::cout << "\n==> itA:" << itA << "\tTemp:" << temp << "\tSum:" << sum << "\n";
+
+    temp *= 0.99;
+    o_ij = m_ij;
+    itA++;
+  }
+  // permite sair dos lacos ao encontrar 1 para cada localizacao
+  out:
+  os.str("");
+  os << path << "/F_mij.txt";
+  PrintMatrix (m_ij, os.str());
+  return m_ij;
+}
+
+std::vector<int> min_conf;
+std::vector<std::vector<long double> > g_b_ij;
+double glob_val = 0;
+
+void permute (std::vector<int> uav_loc, int start, int end, int N) {
+  std::cout << "[" << start << "," << end << "] ";
+  if (start == end) {
+    double val = 0.0;
+    for (int i = 0; i<N; ++i) {
+      std::cout << uav_loc[i] << " ";
+      // val = val + g_b_ij[uav_loc[i]][i];
+      val = val + g_b_ij[i][uav_loc[i]];
+    }
+    std::cout << std::endl;
+    if (val < glob_val) {
+      std::cout << "New value: " << val << std::endl;
+      glob_val = val;
+      for (int i = 0; i<N; ++i) { // copiando
+        min_conf[i] = uav_loc[i];
+      }
+    }
+  } else {
+    for (int i = start; i <= end; ++i) {
+      int aux = uav_loc[i];
+      uav_loc[i] = uav_loc[start];
+      uav_loc[start] = aux;
+      permute(uav_loc, start+1, end, N);
+      // backtrack
+      aux = uav_loc[i];
+      uav_loc[i] = uav_loc[start];
+      uav_loc[start] = aux;
+    }
+  }
+}
+
+void exhaustive (std::vector<std::vector<long double> > b_ij, int N) {
+  for (int i = 0; i<N; ++i) {
+    g_b_ij.push_back(std::vector<long double>());
+    for(int j = 0; j < N; j++)
+    {      
+      g_b_ij[i].push_back(b_ij[i][j]);
+    }
+  }
+  
+  std::vector<int> uav_loc;
+  for (int i = 0; i<N; ++i) {
+    uav_loc.push_back(i);
+    min_conf.push_back(i);
+  }  
+
+  permute(uav_loc, 0, N-1, N);
+
+  std::cout << "Best: ";
+  double t = 0;
+  for (int i = 0; i < N; ++i) {
+    std::cout << min_conf[i] << "[" << b_ij[i][min_conf[i]] << "] ";
+    t += b_ij[i][min_conf[i]];
+  }  
+  std::cout << "---> " << t << std::endl;
+}
